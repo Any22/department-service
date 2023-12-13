e296b797dcd486cf635ea30c3834663b834f252a
saba <sabasaeedakhtar@gmail.com>
2023-12-13 15:56:04 +1100

    added service registry and discovery features

--- src/test/java/com/example/departmentservice/DepartmentServiceApplicationTests.java
+++ src/test/java/com/example/departmentservice/DepartmentServiceApplicationTests.java
@@ -2,8 +2,12 @@ package com.example.departmentservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.example.departmentservice.testconfiguration.TestConfig;
 
 @SpringBootTest
+@ContextConfiguration(classes = TestConfig.class)
 class DepartmentServiceApplicationTests {
 
 	@Test
