1e9943ca2b5676f72403462540715bf429dce85e
saba <sabasaeedakhtar@gmail.com>
2024-06-24 14:29:57 +1000

    added lombok dependency and annoations in entity file

--- src/main/java/com/example/departmentservice/entity/Department.java
+++ src/main/java/com/example/departmentservice/entity/Department.java
@@ -7,58 +7,33 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Entity
 @Table(name="department")
-
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Department {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "department_dept_id_seq")
 	@SequenceGenerator(name = "department_dept_id_seq", sequenceName = "department_dept_id_seq", allocationSize = 1)
 	@Column(name="dept_id")
 	private Long deptId;
+	
 	@Column(name="department_name")
 	private String departmentName;
+	
 	@Column(name="department_address")
 	private String departmentAddress;
+	
 	@Column(name="department_code")
 	private String departmentCode;
-	public Department() {
-		
-	}
-	public Department(Long deptId, String departmentName, String departmentAddress, String departmentCode) {
-		super();
-		this.deptId = deptId;
-		this.departmentName = departmentName;
-		this.departmentAddress = departmentAddress;
-		this.departmentCode = departmentCode;
-	}
-	
-	public Long getDeptId() {
-		return deptId;
-	}
-	public void setDeptId(Long deptId) {
-		this.deptId = deptId;
-	}
-	public String getDepartmentName() {
-		return departmentName;
-	}
-	public void setDepartmentName(String departmentName) {
-		this.departmentName = departmentName;
-	}
-	public String getDepartmentAddress() {
-		return departmentAddress;
-	}
-	public void setDepartmentAddress(String departmentAddress) {
-		this.departmentAddress = departmentAddress;
-	}
-	public String getDepartmentCode() {
-		return departmentCode;
-	}
-	public void setDepartmentCode(String departmentCode) {
-		this.departmentCode = departmentCode;
-	}
 
 	
 }
